b21ccd3649892306d100f5d4ba117df09d060751
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2024-01-16 11:13:09 +0100

    feat: Implement manual entry page with JSON conversion

--- app/manual-entry/page.tsx
+++ app/manual-entry/page.tsx
@@ -2,69 +2,140 @@
 import React, { MouseEvent, useEffect, useState } from "react";
 
 interface FormField {
-  key: string;
-  value: string;
-  [key: string]: string;
+  Name: string;
+  AccountNumber: string;
+  Role: string;
+  BankName: string;
+  Amount: string;
 }
-const initialFormFields: FormField[] = [
-  { key: "Name", value: "" },
-  { key: "AccountNumber", value: "" },
-  { key: "Role", value: "" },
-  { key: "BankName", value: "" },
-  { key: "Amount", value: "" },
-];
+
+const initialFormFields: FormField = {
+  Name: "",
+  AccountNumber: "",
+  Role: "",
+  BankName: "",
+  Amount: "",
+};
 
 export default function Page() {
   const [formFields, setFormFields] = useState<FormField[]>([
-    ...initialFormFields,
+    initialFormFields,
   ]);
-  const [jsonData, setJsonData] = useState<string | null>(null);
 
-  useEffect(() => {
-    const formData: Record<string, string | undefined> = formFields.reduce(
-      (acc, field) => {
-        if (field.key && field.value) {
-          console.log(formFields);
-          acc[field.key] = field.value;
-        }
-        return acc;
-      }
-    );
+  const handleAddField = (e: MouseEvent<HTMLButtonElement>) => {
+    const addNewFields = [...formFields, initialFormFields];
+    setFormFields(addNewFields);
+  };
 
-    setJsonData(JSON.stringify(formData, null, 2));
-    console.log(jsonData);
-  }, [formFields]);
-
-  const handleFieldChange = (index: number, key: string, value: string) => {
+  const handleChange = (index: number, fieldName:string, value: string) => {
     const newFormFields = [...formFields];
-    newFormFields[index] = { key, value };
+    newFormFields[index] = { ...newFormFields[index], [fieldName]: value };
     setFormFields(newFormFields);
   };
 
-  const handleAddField = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setFormFields([...formFields, ...initialFormFields]);
-  };
-
+  console.log(JSON.stringify(formFields, null, 2));
   return (
-    <main className="flex min-h-screen flex-col items-stretch gap-5 p-5 md:p-24 bg-slate-50 text-indigo-900">
+    <main className="flex min-h-screen flex-col items-stretch gap-5 p-5 md:p-24 bg-slate-50 text-teal-900">
       <section className="max-w-2xl w-full mx-auto">
+        <h2>Form Fields:</h2>
         <form>
-          <h2>Form Fields:</h2>
           {formFields.map((field, index) => (
-            <div key={index} className="flex flex-col">
-              <label className={`${field.key == "Name" ? 'mt-6' : 'mt-2'}`}>{field.key}</label>
-              <input
-                type="text"
-                placeholder={field.key}
-                value={field.value}
-                onChange={(e) =>
-                  handleFieldChange(index, field.key, e.target.value)
-                }
-              />
+            <div className="mb-8" key={index}>
+              <p className="text-center">User {index+1}</p>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="Name"
+                  className="text-teal-900 text-xs font-medium block pl-3"
+                >
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  name="Name"
+                  value={formFields[index].Name}
+                  onChange={(e) => handleChange(index, "Name", e.target.value)}
+                  className="border border-solid border-deep-green-50 rounded-[10px] w-full bg-[#FCFFFE] px-3 py-2 text-xs text-deep-green-100 placeholder:text-light-text outline-none placeholder:font-normal font-semibold"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <label
+                  htmlFor="AccountNumber"
+                  className="text-teal-900 text-xs font-medium block pl-3"
+                >
+                  Account Number:
+                </label>
+                <input
+                  type="text"
+                  name="AccountNumber"
+                  value={formFields[index].AccountNumber}
+                  onChange={(e) =>
+                    handleChange(index, "AccountNumber", e.target.value)
+                  }
+                  className="border border-solid border-deep-green-50 rounded-[10px] w-full bg-[#FCFFFE] px-3 py-2 text-xs text-deep-green-100 placeholder:text-light-text outline-none placeholder:font-normal font-semibold"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <label
+                  htmlFor="Role"
+                  className="text-teal-900 text-xs font-medium block pl-3"
+                >
+                  Role:
+                </label>
+                <input
+                  type="text"
+                  name="Role"
+                  value={formFields[index].Role}
+                  onChange={(e) => handleChange(index, "Role", e.target.value)}
+                  className="border border-solid border-deep-green-50 rounded-[10px] w-full bg-[#FCFFFE] px-3 py-2 text-xs text-deep-green-100 placeholder:text-light-text outline-none placeholder:font-normal font-semibold"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <label
+                  htmlFor="BankName"
+                  className="text-teal-900 text-xs font-medium block pl-3"
+                >
+                  Bank Name:
+                </label>
+                <input
+                  type="text"
+                  name="BankName"
+                  value={formFields[index].BankName}
+                  onChange={(e) =>
+                    handleChange(index, "BankName", e.target.value)
+                  }
+                  className="border border-solid border-deep-green-50 rounded-[10px] w-full bg-[#FCFFFE] px-3 py-2 text-xs text-deep-green-100 placeholder:text-light-text outline-none placeholder:font-normal font-semibold"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <label
+                  htmlFor="Amount"
+                  className="text-teal-900 text-xs font-medium block pl-3"
+                >
+                  Amount:
+                </label>
+                <input
+                  type="text"
+                  name="Amount"
+                  value={formFields[index].Amount}
+                  onChange={(e) =>
+                    handleChange(index, "Amount", e.target.value)
+                  }
+                  className="border border-solid border-deep-green-50 rounded-[10px] w-full bg-[#FCFFFE] px-3 py-2 text-xs text-deep-green-100 placeholder:text-light-text outline-none placeholder:font-normal font-semibold"
+                />
+              </div>
             </div>
           ))}
-          <button onClick={handleAddField}>Add Field</button>
+          <button
+            onClick={handleAddField}
+            type="button"
+            className="bg-teal-900 w-full text-center rounded-[10px] px-3 py-2 text-sm font-bold text-white"
+          >
+            Add Field
+          </button>
         </form>
       </section>
     </main>
